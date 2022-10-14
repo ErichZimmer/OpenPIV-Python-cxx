36ab8ab938a63eea4043b0ebc6c3144143f2b9a8
ErichZimmer <Erich_Zimmer@hotmail.com>
2022-10-14 01:34:55 -0500

    Followed Freud-analysis shared libs RPATH

--- lib/__set_path__.py
+++ lib/__set_path__.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
-extra_dll_dir = os.path.join(os.path.dirname(__file__), "_libs")
+# _libs folder is deprecated
+#extra_dll_dir = os.path.join(os.path.dirname(__file__), "_libs")
 
-if os.path.isdir(extra_dll_dir):
-    os.environ.setdefault("PATH", "")
-    os.environ["PATH"] += os.pathsep + extra_dll_dir
+#if os.path.isdir(extra_dll_dir):
+#    os.environ.setdefault("PATH", "")
+#    os.environ["PATH"] += os.pathsep + extra_dll_dir
+
 del os, sys
--- lib/_libs/README.txt
+++
@@ -1,1 +0,0 @@
-This is where dynamic library dependencencies and their liscenses would go. It is added to path by __set_path__.py so this package can find the libraries. Eventually, a wheel patching script would be made to emulate static linking.
