6b6f731aaf2328f40d54782fba17d76f27af0e61
Erich Zimmer <69478071+ErichZimmer@users.noreply.github.com>
2022-10-16 16:10:32 -0500

    Fixed rpath issue

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pytest
 
+from os.path import join
 from openpiv_cxx import validate
 
 # read test data
-data = np.loadtxt("exp1_001.vec", delimiter="\t")
+path = join(__file__, '..', 'exp1_001.vec')
+data = np.loadtxt(path, delimiter="\t")
 
 # extract individual components
 u, v, s2n = data[:,2], data[:,3], data[:,4]
