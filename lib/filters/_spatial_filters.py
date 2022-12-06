c8bbf5854e6c29ab46f195a1ad4514c6c27ded18
ErichZimmer <Erich_Zimmer@hotmail.com>
2022-12-06 12:25:35 -0600

    BUG: Fixed misspelled function

--- lib/filters/_spatial_filters.py
+++ lib/filters/_spatial_filters.py
@@ -26,75 +26,46 @@ Int = np.int32
 kernel_size_error = "kernel_size must be an odd number"
 
 
-def intensity_cap(
-    img, 
-    std_mult = 2.0
+def contrast_stretch(
+    img,
+    lower_limit = 2.0, 
+    upper_limit = 98.0
 ):
-    """Set pixels above calculated threshold to threshold.
-
-    Set pixels higher than calculated threshold to said threshold.
-    The threshold value is calcualted by img_mean + std_mult * img_std.
-    Assumes array is normalized to [0..1].
-
-    Parameters
-    ----------
-    img: 2D float32 array
-        A two dimensional array containing pixel intenensities.
-    std_mult: float
-        Lower values yields a lower threshold.
-
-    Returns
-    -------
-    img: 2D float32 array
-        A filtered two dimensional array of the input image.
-
-    """
-    _check(ndim=2, img=img)
-
-    # store original image dtype
-    img_dtype = img.dtype
-
-    if img_dtype != Float:
-        img = img.astype(Float)
-
-    # cpp version was found to be faster than NumPy
-    new_img = _intensity_cap(img, float(std_mult))
-
-    return new_img
-
-
-def threshold_binarization(
-    img, 
-    threshold = 0.5,
-    out = None
-):
-    """Threshold binarization filter.
+    """Percentile-based contrast stretching.
     
-    A simple threshold binarization filter.
+    Percentile-based contrast stretching similar to MATLAB's imadjust.
     Assumes array is normalized to [0..1].
 
     Parameters
     ----------
     img : 2D float32 array
         A two dimensional array containing pixel intenensities.
-    kernel_size : int
-        nxn size of the convolution kernel.
-    sigma : float
-        sigma of nxn gaussian convolution kernel.
-    keep_dtype : bool
-        Cast output to original dtype.
+    lower_limit: int
+        Lower percentile limit.
+    upper_limit: int
+        Upper percentile limit.
 
     Returns
     -------
-    new_img : 2D float32 array
-        A two dimensional array containing pixel intenensities.
+    img: 2D float32 array
+        A filtered two dimensional array of the input image
 
     """
     _check(ndim=2, img=img)
 
-    # extract filtered image
-    img[img < threshold] = 0
-    img[img > threshold] = 1
+    if lower_limit < 0:
+        lower_limit = 0
+    if upper_limit > 100:
+        upper_limit = 100
+
+    lower = np.percentile(img, lower_limit)
+    upper = np.percentile(img, upper_limit)
+
+    img_max = img.max()
+    img_min = img.min()
+
+    img = np.clip(img, lower, upper)
+    img = (img - lower) / (upper - lower)
 
     return img
 
@@ -164,7 +135,7 @@ def gaussian_filter(
     _check(ndim=2, img=img)
 
     # get 1D kernel
-    kernel = gassian_kernel(
+    kernel = gaussian_kernel(
         sigma=sigma,
         truncate=truncate,
         ndim=1
@@ -223,6 +194,85 @@ def highpass_filter(
     if clip_at_zero == True:
         new_img[new_img < 0] = 0
 
+    return new_img
+
+
+def intensity_cap(
+    img, 
+    std_mult = 2.0
+):
+    """Set pixels above calculated threshold to threshold.
+
+    Set pixels higher than calculated threshold to said threshold.
+    The threshold value is calcualted by img_mean + std_mult * img_std.
+    Assumes array is normalized to [0..1].
+
+    Parameters
+    ----------
+    img : 2D float32 array
+        A two dimensional array containing pixel intenensities.
+    std_mult : float
+        Lower values yields a lower threshold.
+
+    Returns
+    -------
+    img : 2D float32 array
+        A filtered two dimensional array of the input image.
+
+    """
+    _check(ndim=2, img=img)
+
+    # store original image dtype
+    img_dtype = img.dtype
+
+    if img_dtype != Float:
+        img = img.astype(Float)
+
+    # cpp version was found to be faster than NumPy
+    new_img = _intensity_cap(img, float(std_mult))
+
+    return new_img
+
+
+def sobel_filter(
+    img
+):
+    """Simple Sobel filter
+    Simple 2D Sobel filter by convolution of 1D arrays.
+    
+    Parameters
+    ----------
+    img : 2D float32 array
+        A two dimensional array containing pixel intenensities.
+    
+    Returns
+    -------
+    new_img : 2D float32 array
+        A two dimensional array containing pixel intenensities.
+    
+    """
+    
+    sobel_h = np.array([-1, 0, 1], dtype = Float)
+    sobel_v = np.array([ 1, 2, 1], dtype = Float)
+    sobel_h /= 3
+    sobel_v /= 3
+    
+    img1 = convolve_2D_sep(
+        img,
+        sobel_h,
+        sobel_v
+    )
+    
+    # Use this as the output image to avoid more temporary arrays
+    new_img = convolve_2D_sep(
+        img,
+        sobel_v,
+        sobel_h
+    )
+    
+    # consolidate the two images
+    np.hypot(img1, new_img, out = new_img)
+    
     return new_img
 
 
@@ -285,89 +335,39 @@ def variance_normalization_filter(
     new_img = (new_img - new_img.min()) / (new_img.max() - new_img.min())
     
     return new_img
+    
 
-
-def sobel_filter(
-    img
+def threshold_binarization(
+    img, 
+    threshold = 0.5,
+    out = None
 ):
-    """Simple Sobel filter
-    Simple 2D Sobel filter by convolution of 1D arrays.
+    """Threshold binarization filter.
     
-    Parameters
-    ----------
-    img : 2D float32 array
-        A two dimensional array containing pixel intenensities.
-    
-    Returns
-    -------
-    new_img : 2D float32 array
-        A two dimensional array containing pixel intenensities.
-    
-    """
-    
-    sobel_h = np.array([-1, 0, 1], dtype = Float)
-    sobel_v = np.array([ 1, 2, 1], dtype = Float)
-    sobel_h /= 3
-    sobel_v /= 3
-    
-    img1 = convolve_2D_sep(
-        img,
-        sobel_h,
-        sobel_v
-    )
-    
-    # Use this as the output image to avoid more temporary arrays
-    new_img = convolve_2D_sep(
-        img,
-        sobel_v,
-        sobel_h
-    )
-    
-    # consolidate the two images
-    np.hypot(img1, new_img, out = new_img)
-    
-    return new_img
-    
-    
-def contrast_stretch(
-    img,
-    lower_limit = 2.0, 
-    upper_limit = 98.0
-):
-    """Percentile-based contrast stretching.
-    
-    Percentile-based contrast stretching similar to MATLAB's imadjust.
+    A simple threshold binarization filter.
     Assumes array is normalized to [0..1].
 
     Parameters
     ----------
     img : 2D float32 array
         A two dimensional array containing pixel intenensities.
-    lower_limit: int
-        Lower percentile limit.
-    upper_limit: int
-        Upper percentile limit.
+    kernel_size : int
+        nxn size of the convolution kernel.
+    sigma : float
+        sigma of nxn gaussian convolution kernel.
+    keep_dtype : bool
+        Cast output to original dtype.
 
     Returns
     -------
-    img: 2D float32 array
-        A filtered two dimensional array of the input image
+    new_img : 2D float32 array
+        A two dimensional array containing pixel intenensities.
 
     """
     _check(ndim=2, img=img)
 
-    if lower_limit < 0:
-        lower_limit = 0
-    if upper_limit > 100:
-        upper_limit = 100
-
-    lower = np.percentile(img, lower_limit)
-    upper = np.percentile(img, upper_limit)
-
-    img_max = img.max()
-    img_min = img.min()
-
-    img = np.clip(img, lower, upper)
-    img = (img - lower) / (upper - lower)
+    # extract filtered image
+    img[img < threshold] = 0
+    img[img > threshold] = 1
 
     return img
