26202de74ac5d2c973b793203f8d268aab47729d
ErichZimmer <Erich_Zimmer@hotmail.com>
2023-02-09 22:40:30 -0600

    ENH: Remove dependency on scipy when smoothing

--- tests/test_smooth.py
+++ tests/test_smooth.py
@@ -13,11 +13,40 @@ u = data[:,2]
 u = u.reshape(29,41)
 
 
-def test_smooth_spline_no_mask():
-    u_smth = smooth.smooth_spline(u, s=1)
-
-
-def test_smooth_spline_blank_mask():
-    u_masked = np.ma.MaskedArray(u, mask=np.ma.nomask)
+def test_smooth_gaussian_wrong_inputs():
+    dummy = np.random.rand(32,32,32)
     
-    u_smth = smooth.smooth_spline(u_masked, s=1)
+    with pytest.raises(ValueError):
+        # Not 2D
+        u_smth = smooth.smooth_gaussian(
+            dummy
+        )
+        
+        # test for NaNs
+        u2 = u.copy()
+        u2[-3, -3] = np.nan
+        
+        u_smth = smooth.smooth_gaussian(
+            u2
+        )
+
+
+def test_smooth_gaussian_01():
+    u_smth = smooth.smooth_gaussian(
+        u
+    )
+
+
+def test_smooth_gaussian_02():
+    u_smth = smooth.smooth_gaussian(
+        u,
+        sigma=1.0,
+        truncate=2.0
+    )
+    
+    
+def test_smooth_gaussian_03():
+    u_smth = smooth.smooth_gaussian(
+        u,
+        half_width=1
+    )
